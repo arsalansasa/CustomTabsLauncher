fae44f9d49a4bd229196895d4eb6d86526c349bf
arsalan <arsalansky58@gmail.com>
2022-01-31 19:11:48 +0330

    CustomgetPackageName implemented to make sure samsung browser wouldn't launch

--- launcher/src/main/java/com/droibit/android/customtabs/launcher/internal/CustomTabsLauncherImpl.kt
+++ launcher/src/main/java/com/droibit/android/customtabs/launcher/internal/CustomTabsLauncherImpl.kt
@@ -5,8 +5,28 @@ import android.net.Uri
 import androidx.browser.customtabs.CustomTabsClient
 import androidx.browser.customtabs.CustomTabsIntent
 import com.droibit.android.customtabs.launcher.CustomTabsFallback
+import android.text.TextUtils
+
+import androidx.browser.customtabs.CustomTabsService.ACTION_CUSTOM_TABS_CONNECTION
+
+import android.content.Intent
+
+import android.content.pm.ResolveInfo
+
+import android.content.pm.PackageManager
+import androidx.browser.customtabs.CustomTabsService
+import android.content.IntentFilter
+import android.util.Log
+import java.lang.RuntimeException
+
 
 internal class CustomTabsLauncherImpl {
+    val STABLE_PACKAGE = "com.android.chrome"
+    val BETA_PACKAGE = "com.chrome.beta"
+    val DEV_PACKAGE = "com.chrome.dev"
+    val LOCAL_PACKAGE = "com.google.android.apps.chrome"
+    private val TAG = "CustomTabsLauncherImpl"
+
     fun launch(
         context: Context,
         customTabsIntent: CustomTabsIntent,
@@ -14,12 +34,91 @@ internal class CustomTabsLauncherImpl {
         expectCustomTabsPackages: List<String>,
         fallback: CustomTabsFallback?
     ) {
-        val customTabsPackage = CustomTabsClient.getPackageName(context, expectCustomTabsPackages)
+        val customTabsPackage = customGetPackageName(context)
         if (customTabsPackage == null && fallback != null) {
             fallback.openUrl(context, uri, customTabsIntent)
             return
         }
         customTabsIntent.intent.setPackage(customTabsPackage)
         customTabsIntent.launchUrl(context, uri)
+    }
+
+    fun customGetPackageName(context:Context) : String? {
+        var sPackageNameToUse : String? = null
+        val pm: PackageManager = context.getPackageManager()
+        // Get default VIEW intent handler.
+        // Get default VIEW intent handler.
+        val activityIntent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.example.com"))
+        val defaultViewHandlerInfo = pm.resolveActivity(activityIntent, 0)
+        var defaultViewHandlerPackageName: String? = null
+        if (defaultViewHandlerInfo != null) {
+            defaultViewHandlerPackageName = defaultViewHandlerInfo.activityInfo.packageName
+        }
+
+        // Get all apps that can handle VIEW intents.
+
+        // Get all apps that can handle VIEW intents.
+        val resolvedActivityList = pm.queryIntentActivities(activityIntent, 0)
+        val packagesSupportingCustomTabs: MutableList<String> = ArrayList()
+        for (info in resolvedActivityList) {
+            val serviceIntent = Intent()
+            serviceIntent.action = ACTION_CUSTOM_TABS_CONNECTION
+            serviceIntent.setPackage(info.activityInfo.packageName)
+            if (pm.resolveService(serviceIntent, 0) != null) {
+                packagesSupportingCustomTabs.add(info.activityInfo.packageName)
+            }
+        }
+
+        if (defaultViewHandlerPackageName == "com.sec.android.app.sbrowser"){
+            defaultViewHandlerPackageName = STABLE_PACKAGE
+        }
+        if(packagesSupportingCustomTabs.contains("com.sec.android.app.sbrowser")){
+            packagesSupportingCustomTabs.remove("com.sec.android.app.sbrowser")
+        }
+        // Now packagesSupportingCustomTabs contains all apps that can handle both VIEW intents
+        // and service calls.
+
+        // Now packagesSupportingCustomTabs contains all apps that can handle both VIEW intents
+        // and service calls.
+        if (packagesSupportingCustomTabs.contains(STABLE_PACKAGE)) {
+            sPackageNameToUse = STABLE_PACKAGE
+        } else if (packagesSupportingCustomTabs.contains(BETA_PACKAGE)) {
+            sPackageNameToUse = BETA_PACKAGE
+        } else if (packagesSupportingCustomTabs.contains(DEV_PACKAGE)) {
+            sPackageNameToUse = DEV_PACKAGE
+        } else if (packagesSupportingCustomTabs.contains(LOCAL_PACKAGE)) {
+            sPackageNameToUse = LOCAL_PACKAGE
+        } else if (packagesSupportingCustomTabs.isEmpty()) {
+            sPackageNameToUse = STABLE_PACKAGE
+        } else if (packagesSupportingCustomTabs.size == 1) {
+            sPackageNameToUse = packagesSupportingCustomTabs[0]
+        } else if (!TextUtils.isEmpty(defaultViewHandlerPackageName)
+                && !hasSpecializedHandlerIntents(context, activityIntent)
+                && packagesSupportingCustomTabs.contains(defaultViewHandlerPackageName)) {
+            sPackageNameToUse = defaultViewHandlerPackageName
+        }
+        return sPackageNameToUse
+
+    }
+
+    private fun hasSpecializedHandlerIntents(context: Context, intent: Intent): Boolean {
+        try {
+            val pm = context.packageManager
+            val handlers = pm.queryIntentActivities(
+                    intent,
+                    PackageManager.GET_RESOLVED_FILTER)
+            if (handlers.size == 0) {
+                return false
+            }
+            for (resolveInfo in handlers) {
+                val filter = resolveInfo.filter ?: continue
+                if (filter.countDataAuthorities() == 0 || filter.countDataPaths() == 0) continue
+                if (resolveInfo.activityInfo == null) continue
+                return true
+            }
+        } catch (e: RuntimeException) {
+            Log.e(TAG, "Runtime exception while getting specialized handlers")
+        }
+        return false
     }
 }
